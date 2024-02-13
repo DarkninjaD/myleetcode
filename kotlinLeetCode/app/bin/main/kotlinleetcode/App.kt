0d41a0300f0b9f050c9572a2fc877c0f96d2fd70
d.sauerbronn <linkdayan2000@gmail.com>
2024-02-13 11:35:41 -0600

    don't recall what change but need to back up

--- kotlinLeetCode/app/bin/main/kotlinleetcode/App.kt
+++ kotlinLeetCode/app/bin/main/kotlinleetcode/App.kt
@@ -4,6 +4,10 @@
 package kotlinleetcode
 import java.net.DatagramPacket
 import java.net.DatagramSocket
+import java.net.InetAddress
+import kotlin.csv.csv
+
+
 
 class App {
     val greeting: String
@@ -12,25 +16,37 @@ class App {
         }
 }
 
+import java.io.File
 fun main() {
-
-fun main(args: Array<String>) {
-  // Create a DatagramSocket
-  val socket = DatagramSocket()
-
-  // Create a DatagramPacket
-  val packet = DatagramPacket(
-    "Hello, world!".toByteArray(),
-    "Hello, world!".toByteArray().size,
-    "127.0.0.1",
-    8080
-  )
-
-  // Send the packet
-  socket.send(packet)
-
-  // Close the socket
-  socket.close()
+    var myFilePath = "YOUR/FILE/HERE.WHATEVER"
+    var fileContent = readFromFile(File(myFilePath))
+    println(fileContent)
+}
+fun readFromFile (filepath : File) : String {
+    return if(filepath.exists()) {
+        filepath.readText()
+    } else {
+        "File Does not exist"
+    }
 }
 
-}
+fun udpOverLocal(args: Array<String>) {
+    // Create a DatagramSocket
+    val socket = DatagramSocket()
+
+    //   val address : InetAddress = 123.0.0.1
+    // Create a DatagramPacket
+    val packet = DatagramPacket(ByteArray(1024), 1024
+        // "Hello, world!".toByteArray(),
+        // 0,
+        // "Hello, world!".toByteArray().size,
+        // 127.0.0.1,
+        // 8080
+    )
+
+    // Send the packet
+    socket.send(packet)
+
+    // Close the socket
+    socket.close()
+    }
